7b9635126a57da0ac342c9c5a09704a6f9f4fe31
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-03-24 00:13:10 -0300

    flat component done and working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,13 +4,25 @@ import flats from '../../data/flats';
 import FlatList from './flat_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    }
+  }
 
-
+  selectFlat(index) {
+    this.setstate({ selectFlat: flats[index] });
+  }
 
   render() {
     return (
       <div>
-        <FlatList flat={}
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
         />
       </div>
     );
--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 
 class Flat extends Component {
 
+  handleClick = () => {
+    this.props.selectFlat(this.props.index)
+  }
+
   render() {
     return (
-
+      <div class={ `card${this.props.selected ? ' active' : ''}`} style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${this.props.flat.imageUrl}')` }}>
+        <div class="card-category">{this.props.flat.price} { this.props.flat.priceCurrency }</div>
+        <div class="card-description">
+          <h2>{ this.props.flat.name }</h2>
+        </div>
+        <a class="card-link" href="#" onClick={ this.handleClick }></a>
+      </div>
     );
   }
 }
