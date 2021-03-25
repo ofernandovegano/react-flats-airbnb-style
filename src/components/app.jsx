05eba1d4b378c131d242e63385a22e39e308e739
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-03-25 19:08:04 -0300

    map fixed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-require('dotenv').config()
 import GoogleMapReact from 'google-map-react';
 
 import flats from '../../data/flats';
@@ -8,31 +7,34 @@ import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       selectedFlat: flats[0],
       flats
-    }
+    };
   }
 
-  selectFlat(index) {
-    this.setstate({ selectFlat: flats[index] });
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
   }
 
   render() {
     return (
       <div>
-        <FlatList
-          flats={this.state.flats}
-          selectedFlat={this.state.selectedFlat}
-          selectFlat={this.selectFlat}
-        />
+          <FlatList
+            flats={this.state.flats}
+            selectedFlat={this.state.selectedFlat}
+            selectFlat={this.selectFlat}
+            />
         <div className="map-container">
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API}}
-            defaultCenter={this.center()}
-            defaultZoom={12}
-          >
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
             <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
           </GoogleMapReact>
         </div>
